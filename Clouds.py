8ffa6d13f8a8cbd5e784137df57d9513e7f8888d
bilydaniel <bilidanek@seznam.cz>
2023-10-17 18:16:25 +0200

    added clouds

---
+++ Clouds.py
@@ -0,0 +1,37 @@
+import random
+
+
+class Cloud:
+    def __init__(self, pos, speed, depth, img):
+        self.pos = list(pos)
+        self.speed = speed
+        self.depth = depth
+        self.img = img
+
+    def update(self):
+        self.pos[0] += self.speed
+
+    def render(self, surf, offset=(0, 0)):
+        render_pos = (self.pos[0] - offset[0] * self.depth, self.pos[1] - offset[1] * self.depth)
+        surf.blit(self.img, (render_pos[0] % (surf.get_width() + self.img.get_width()) - self.img.get_width(),
+                             render_pos[1] % (surf.get_height() + self.img.get_height()) - self.img.get_height()))
+
+class Clouds:
+    def __init__(self, images, number_of_clouds=16):
+        self.clouds = []
+        for i in range(number_of_clouds):
+            position = (random.random() * 99999, random.random() * 99999)
+            img = images #TODO poslat vic obrazku, vybrat jeden
+            speed = random.random() * 0.05 + 0.05
+            depth = random.random() * 0.6 + 0.2
+            self.clouds.append(Cloud(position, speed, depth, img))
+
+        self.clouds.sort(key=lambda x: x.depth)
+
+    def update(self):
+        for cloud in self.clouds:
+            cloud.update()
+
+    def render(self, surf, offset=(0, 0)):
+        for cloud in self.clouds:
+            cloud.render(surf, offset=offset)
--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import sys
 from tilemap import *
 from player import *
 from helpers import *
+from Clouds import *
 
 class Game:
     def __init__(self):
@@ -15,10 +16,13 @@ class Game:
         self.player = Player(self, (50,50))
         self.movement = [False, False]
 
+
         self.assets = {
             "player": load_image('player.png'),
-            "background": load_image("background.png")
+            "background": load_image("background.png"),
+            "clouds": load_image("cloud.png")
         }
+        self.clouds = Clouds(self.assets['clouds'])
 
     def run(self):
         while True:
@@ -47,11 +51,13 @@ class Game:
 
     def update(self):
         self.player.update((self.movement[RIGHT] - self.movement[LEFT],0))
+        self.clouds.update()
 
     def render(self):
         self.display.blit(self.assets['background'], (0,0))
         self.tilemap.render(self.display)
         self.player.render(self.display)
+        self.clouds.render(self.display)
         self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
 
         pygame.display.update()
